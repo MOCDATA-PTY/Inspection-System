ec78767ab9f5295c84342af7770be7d6b97b4526
MOCDATA-PTY <noreply@github.com>
2025-12-05 00:35:21 -0800

    Remove test_view import that was moved to tests folder
    - Removed import from test_view module (moved to tests/)
    - Removed test URL patterns
    - Added standard Django media file serving for development
    - Fixes ModuleNotFoundError on startup

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-from test_view import test_media_file, serve_media_file
 
 def redirect_to_login(request):
     return redirect('login')
@@ -11,8 +10,9 @@ def redirect_to_login(request):
 urlpatterns = [
     path('', redirect_to_login),  # Redirect root URL to login
     path('admin/', admin.site.urls),
-    path('test-media/', test_media_file, name='test_media_file'),  # Test view
     path('', include('main.urls')),
-    # Serve media files with custom view
-    re_path(r'^media/(?P<path>.*)$', serve_media_file, name='serve_media'),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
